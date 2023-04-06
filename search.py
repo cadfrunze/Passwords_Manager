f0f6efaf636e9cdff34498993adb1e3b32405568
cadfrunze <marius1890@yahoo.com>
2023-04-06 23:22:58 +0300

    update

--- search.py
+++ search.py
@@ -16,9 +16,8 @@ def search_fun(obiect: str):
                 website_str = ''.join(elem)
                 email_str = ''.join(all_data[elem]['email'])
                 password_str = ''.join(all_data[elem]['password'])
-                if len(password_str) > 9 or len(email_str) > 9 or len(website_str) > 9:
-                    messagebox.showinfo(title='Date gasite',
-                                        message=f'1. Website: {website_str}\n2. Email: {email_str}\n3. Password: {password_str}')
+                messagebox.showinfo(title='Date gasite',
+                                    message=f'1. Website: {website_str}\n2. Email: {email_str}\n3. Password: {password_str}')
                 break
         else:
             messagebox.showinfo(title='Date negasite', message=f'Nu am gasit date despre acest website: "{obiect}"')

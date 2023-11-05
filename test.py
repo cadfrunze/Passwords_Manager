a9ab23b06559c61af673fc74a810da88767973f9
Marius <marius1890@yahoo.com>
2023-11-05 20:16:30 +0200

    update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-with open('work_log.txt', 'a') as file:
-    file.writelines('26.03.2023\n')
-    file.writelines('27.03\n')
-    file.writelines('28.03\n')
-    file.writelines('29.03\n')
+from datetime import datetime
+
+data_editare = datetime.now().strftime("Data: %d/%m/%Y | Ora: %H:%M:%S")
+
+with open('./work_log.txt','a') as fisier:
+    fisier.writelines('\n' + data_editare)
